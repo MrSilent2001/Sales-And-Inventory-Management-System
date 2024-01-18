541a0be1637146278adcf0473cf2b64ce52ff732
KavithRashintha <wgkrashintha@gmail.com>
2024-01-18 09:32:15 +0530

    Navigation Bar Created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import InventoryLanding from "./pages/inventory landing page/inventoryLanding";
+import Navbar from "./layout/navbar/navbar";
 
 function App() {
   return (
     <div className="App">
+        <Navbar></Navbar>
       <InventoryLanding></InventoryLanding>
     </div>
   );
---
+++ src/layout/navbar/navbar.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./navbar.css";
+
+function navbar(){
+    return(
+        <div className="navbar">
+            <div className="logo">
+                <img src="./logo.png" alt="Logo" />
+            </div>
+            <div className="navigation">
+                <a href="#">Inventory</a>
+                <a href="#">Supplier</a>
+                <a href="#">Orders</a>
+                <a href="#">Refunds</a>
+                <a href="#">Payment</a>
+                <a href="#" className="lastNavLink">Logout</a>
+            </div>
+        </div>
+    )
+}
+
+export default navbar;
