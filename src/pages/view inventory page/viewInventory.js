a1a30abf8efa73bc61a5932be517fee594c2c8a4
KavithRashintha <wgkrashintha@gmail.com>
2024-01-22 21:40:55 +0530

    Add and Delete Buttons Created

--- src/pages/view inventory page/viewInventory.js
+++ src/pages/view inventory page/viewInventory.js
@@ -1,6 +1,9 @@
 import React from "react";
 import Box from '@mui/material/Box';
+import { styled } from '@mui/material/styles';
 import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import { purple } from '@mui/material/colors';
 import "./viewInventory.css";
 
 function SearchBar(){
@@ -10,28 +13,51 @@ function SearchBar(){
             sx={{
                 '& > :not(style)': {
                     m: 1,
-                    width: '55ch',
+                    width: '17.5em',
                     "& .MuiInputBase-root":{
-                        height: 30,
-                        borderRadius: 10,
+                        height: '1.95em',
+                        borderRadius: '1.5em',
                     },
                     "& .MuiInputLabel-root": {
-                        fontSize: 10
+                        fontSize: '0.6em',
+                        textAlign: 'center',
                     },
                 },
             }}
             noValidate
             autoComplete="off"
         >
-            <TextField id="standard-basic" label="Standard" variant="outlined" size="small"/>
+            <TextField id="standard-basic" label="Search Here" variant="outlined" size="small"/>
         </Box>
     )
 }
 
+const AddButton = styled(Button)(({ theme }) => ({
+    color: theme.palette.getContrastText('#242F9B'),
+    backgroundColor: '#242F9B',
+    '&:hover': {
+        backgroundColor: '#242F9B' // You can adjust the darken value as needed
+    },
+}));
+
+const DeleteButton = styled(Button)(({ theme }) => ({
+    color: theme.palette.getContrastText('#D41400'),
+    backgroundColor: '#D41400',
+    '&:hover': {
+        backgroundColor: '#D41400' // You can adjust the darken value as needed
+    },
+}));
+
 function ViewInventory(){
     return(
         <div className="viewInventory">
-            <SearchBar></SearchBar>
+            <div className="searchAndButtons">
+                <SearchBar></SearchBar>
+                <div className="viewInventoryButtons">
+                    <AddButton>Add Item</AddButton>
+                    <DeleteButton>Delete Item</DeleteButton>
+                </div>
+            </div>
         </div>
     )
 }
