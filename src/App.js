dbf7e50fee50e18b28ad241ceb49dd383940b234
MrSilent2001 <bandarigodadm.21@uom.lk>
2024-01-23 22:56:37 +0530

    update supplier page created

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Navbar from "./layout/navbar/navbar";
 import AddDistcounts from "./pages/add Discounts/AddDistcounts";
 import AddSupplier from "./pages/add Supplier/addSupplier";
 import InventoryLanding from "./pages/inventory landing page/inventoryLanding";
+import UpdateSupplier from "./pages/update Supplier/updateSupplier";
+import DiscountDashboard from "./pages/discount Dashboard/discountDashboard";
 
 function App() {
   return (
     <div className="App">
-        <Navbar></Navbar>
-        <AddSupplier/>
+        <Navbar/>
+        <UpdateSupplier/>
     </div>
   );
 }
