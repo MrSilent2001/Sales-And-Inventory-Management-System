cae4210d9564f6f16c3ac9adc91f6fbc38fecdd7
KavithRashintha <wgkrashintha@gmail.com>
2024-01-23 10:48:11 +0530

    View Invetory's Table Created

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Navbar from "./layout/navbar/navbar";
 import Footer from "./layout/footer/footer";
 import ViewInventory from "./pages/view inventory page/viewInventory";
+import CustomizedTables from "./pages/table";
 
 function App() {
   return (
--- src/pages/view inventory page/viewInventory.js
+++ src/pages/view inventory page/viewInventory.js
@@ -10,6 +10,13 @@ import AddItemButton from "../../layout/buttons/addItemButton/AddItemButton";
 import DeleteItemButton from "../../layout/buttons/deleteItemButton/DeleteItemButton";
 import {styled} from "@mui/material/styles";
 import Button from "@mui/material/Button";
+import TableCell, {tableCellClasses} from "@mui/material/TableCell";
+import TableRow from "@mui/material/TableRow";
+import TableContainer from "@mui/material/TableContainer";
+import Paper from "@mui/material/Paper";
+import Table from "@mui/material/Table";
+import TableHead from "@mui/material/TableHead";
+import TableBody from "@mui/material/TableBody";
 
 
 function SearchBar(){
@@ -78,9 +85,56 @@ function FilterItems(){
                         },
                     }}
                 >
-                    <MenuItem value={10}>Ten</MenuItem>
-                    <MenuItem value={20}>Twenty</MenuItem>
-                    <MenuItem value={30}>Thirty</MenuItem>
+                    <MenuItem value={10} >All</MenuItem>
+                    <MenuItem value={20}>Metal</MenuItem>
+                    <MenuItem value={30}>Wood</MenuItem>
+                </Select>
+            </FormControl>
+        </Box>
+    )
+}
+
+function FilterAvailability(){
+
+    const [age, setAge] = React.useState('');
+
+    const handleChange = (event) => {
+        setAge(event.target.value);
+    };
+
+    return(
+        <Box sx={{ minWidth: 80 }}>
+            <FormControl fullWidth>
+                <InputLabel
+                    id="demo-simple-select-label"
+                    sx={{
+                        fontSize: '10px',
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        color: 'rgba(255,255,255,0.7)'
+                    }}
+                >
+                    Select
+                </InputLabel>
+                <Select
+                    labelId="demo-simple-select-label"
+                    id="demo-simple-select"
+                    value={age}
+                    label="Age"
+                    onChange={handleChange}
+                    sx={{
+                        height: 40,
+                        width: 160,
+                        fontSize: 10,
+                        border: '1px solid white',
+                        '& .MuiInputLabel-root': {
+                            fontSize: 4, // Set the desired font size for the placeholder
+                        },
+                    }}
+                >
+                    <MenuItem value={10} >All</MenuItem>
+                    <MenuItem value={20}>In Stock</MenuItem>
                 </Select>
             </FormControl>
         </Box>
@@ -102,6 +156,94 @@ const ApplyButton = styled(Button)(({ theme }) => ({
     padding: '1.75em 0.625em'
 }));
 
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+    [`&.${tableCellClasses.head}`]: {
+        backgroundColor:'#646FD4',
+        color: theme.palette.common.white,
+        fontSize: '0.75em',
+        fontWeight: 500,
+        textAlign: 'center'
+    },
+    [`&.${tableCellClasses.body}`]: {
+        fontSize: '0.625em',
+        textAlign: 'center'
+    },
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+    '&:nth-of-type(odd)': {
+        backgroundColor: theme.palette.action.hover,
+    },
+    // hide last border
+    '&:last-child td, &:last-child th': {
+        border: 0,
+    },
+}));
+
+function createData(inventoryId, itemDescription, itemCategory, Quantity, inventoryStatus) {
+    return { inventoryId, itemDescription, itemCategory, Quantity, inventoryStatus };
+}
+
+const rows = [
+    createData('I0001', 'Tokyo Super Cement', 'Cement', 24, 'In Stock'),
+    createData('I0001', 'Tokyo Super Cement', 'Cement', 37, 'In Stock'),
+    createData('I0001', 'Tokyo Super Cement', 'Cement', 24, 'In Stock'),
+    createData('I0001', 'Tokyo Super Cement', 'Cement', 67, 'In Stock'),
+    createData('I0001', 'Tokyo Super Cement', 'Cement', 49, 'In Stock'),
+    createData('I0001', 'Tokyo Super Cement', 'Cement', 49, 'In Stock'),
+    createData('I0001', 'Tokyo Super Cement', 'Cement', 49, 'In Stock'),
+    createData('I0001', 'Tokyo Super Cement', 'Cement', 49, 'In Stock'),
+    createData('I0001', 'Tokyo Super Cement', 'Cement', 49, 'In Stock'),
+];
+
+const ViewItemButton = styled(Button)(({ theme }) => ({
+    color: theme.palette.getContrastText('#242F9B'),
+    backgroundColor: '#242F9B',
+    '&:hover': {
+        backgroundColor: '#2d3ed2' // You can adjust the darken value as needed
+    },
+    '&.MuiButton-root': {
+        width: '11.625em',
+        height: '2.75em'
+    },
+    fontSize: '0.7em',
+    fontFamily: 'inter',
+    padding: '1.75em 0.625em'
+}));
+
+function CustomizedTables() {
+    return (
+        <TableContainer component={Paper} sx={{ width: '76.875em', maxHeight: '25em', overflowY: 'auto', position: 'relative'}}>
+            <Table sx={{ minWidth: '25em'}} aria-label="customized table">
+                <TableHead>
+                    <TableRow>
+                        <StyledTableCell>Inventory ID</StyledTableCell>
+                        <StyledTableCell>Item Description</StyledTableCell>
+                        <StyledTableCell>Item Category</StyledTableCell>
+                        <StyledTableCell>Quantity</StyledTableCell>
+                        <StyledTableCell>Inventory Status</StyledTableCell>
+                        <StyledTableCell></StyledTableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {rows.map((row) => (
+                        <StyledTableRow key={row.name}>
+                            <StyledTableCell component="th" scope="row" sx={{ height: '1.25em' }}>
+                                {row.inventoryId}
+                            </StyledTableCell>
+                            <StyledTableCell align="right">{row.itemDescription}</StyledTableCell>
+                            <StyledTableCell align="right">{row.itemCategory}</StyledTableCell>
+                            <StyledTableCell align="right">{row.Quantity}</StyledTableCell>
+                            <StyledTableCell align="right">{row.inventoryStatus}</StyledTableCell>
+                            <StyledTableCell><ViewItemButton>View</ViewItemButton></StyledTableCell>
+                        </StyledTableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
+    );
+}
+
 function ViewInventory(){
     return(
         <div className="viewInventoryOuter">
@@ -118,7 +260,7 @@ function ViewInventory(){
                         <div className="itemAbailabilityTopic">
                             <h5>Availability</h5>
                         </div>
-                        <FilterItems></FilterItems>
+                        <FilterAvailability></FilterAvailability>
                     </div>
                     <div className="applyButton">
                         <ApplyButton>Apply</ApplyButton>
@@ -126,6 +268,7 @@ function ViewInventory(){
                 </div>
             </div>
             <div className="viewInventoryInner">
+
                 <div className="searchAndButtons">
                     <div className="viewInventorySearch">
                         <SearchBar></SearchBar>
@@ -134,6 +277,10 @@ function ViewInventory(){
                         <AddItemButton>Add Item</AddItemButton>
                         <DeleteItemButton>Delete Item</DeleteItemButton>
                     </div>
+                </div>
+
+                <div className="itemTable">
+                    <CustomizedTables></CustomizedTables>
                 </div>
             </div>
         </div>
