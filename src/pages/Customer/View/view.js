3c3136dc066f640ad9fd7dbbe3efa9991f44542a
Kulathunga D.M.D.T. <kulathungadmdt.21@uom.lk>
2024-03-26 16:31:35 +0530

    dependancies added

--- src/pages/Customer/View/view.js
+++ src/pages/Customer/View/view.js
@@ -26,6 +26,34 @@ const ViewOrdersButtons = styled(Button)(({ theme }) => ({
 
 }));
 
+function SearchBar(){
+    return(
+        <Box
+            component="form"
+            sx={{
+                '& > :not(style)': {
+                    m: 1,
+                    width: '17.5em',
+                    "& .MuiInputBase-root":{
+                        height: '1.95em',
+                        borderRadius: '1.5em',
+                        /*backgroundColor: 'white'*/
+                    },
+                    "& .MuiInputLabel-root": {
+                        fontSize: '0.6em',
+                        textAlign: 'center',
+                    },
+                },
+            }}
+            noValidate
+            autoComplete="off"
+        >
+            <TextField id="standard-basic" label="Search Here" variant="outlined" size="small"/>
+        </Box>
+    )
+}
+
+
 function BasicTextFields() {
     return (
         <Box
