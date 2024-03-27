465e6e84427d5aa0c7672879d68a69680b36c3b3
MrSilent2001 <bandarigodadm.21@uom.lk>
2024-03-27 17:36:06 +0530

    Routing Modified

--- src/routes/PageRoutes.js
+++ src/routes/PageRoutes.js
@@ -1,12 +1,8 @@
-
 import CustomerHome from "../pages/Customer/Home/Customer Home Page";
 import Login from "../pages/login & SignUp/login/login";
 import {Route, Routes} from "react-router-dom";
 import Signup from "../pages/login & SignUp/SignUp/signup";
 import SalesLanding from "../pages/admin/Landing Pages/Sales Landing/salesLanding";
-import DiscountDashboard from "../pages/admin/Discounts/discountDashboard";
-import PaymentDashboard from "../pages/admin/Payment Dashboard/paymentDashboard";
-import CustomerRefunds from "../pages/Customer/Refund/Refund Dashboard/Customer Refunds";
 import CustomerDashboard from "../pages/admin/Customer Dashboard/customerDashboard";
 import CustomerProfile from "../pages/Customer/Customer Profile/Profile/customerProfile";
 import CustomerOrderHistory from "../pages/Customer/Order History/customerOrderHistory";
@@ -14,11 +10,7 @@ import RemoveCustomers from "../pages/admin/Remove Customers/removeCustomers";
 import UpdateCustomers from "../pages/admin/Update Customers/updateCustomers.js";
 import View from "../pages/Customer/View/view";
 import InventoryLanding from "../pages/admin/Landing Pages/Inventory Landing/inventoryLanding";
-import ViewInventory from "../pages/admin/View Inventory/viewInventory";
-import ViewSupplier from "../pages/admin/Supplier Dashboard/viewSupplier";
 import PurchaseOrderDashboard from "../pages/admin/Orders/Inventory/Order Dashboard/PurchaseOrderDashboard";
-import SalesRefundRequestsTable from "../pages/admin/Refunds/Customer/View Sales Refund Requests/SalesRefundRequestsTable";
-import Cart from "../pages/Customer/Cart/shoppingCart";
 import ApprovedRefundsTable from "../pages/admin/Refunds/Customer/Approved Refunds/ApprovedRefundsTable";
 import InventoryGeneratedRequest from "../pages/admin/Refunds/Customer/Generated Refund Request/SalesViewRequest";
 import InventoryRefundRequest from "../pages/admin/Refunds/Inventory/InventoryRefundRequest/InventoryRefundRequest";
@@ -27,10 +19,6 @@ import RefundsManager from "../pages/admin/Refunds/Inventory/RefundRequestsTable
 import SalesApprovedRefundsTable from "../pages/admin/Refunds/Customer/Sales_Approved Refunds/SalesApprovedRefundsTable";
 import SalesRefundDenialForm from "../pages/admin/Refunds/Customer/Refund Denial Form/SalesRefundDenialForm";
 import SalesViewRequest from "../pages/admin/Refunds/Customer/Generated Refund Request/SalesViewRequest";
-import Catalog from "../pages/Customer/Product Catelog/catalog";
-import PendingOrders from "../pages/admin/Orders/Sales/Pending Orders/pendingOrders";
-import InventoryDashboard from "../pages/Supplier/Inventory Dashboard/inventoryDashboard";
-import SupplierProfile from "../pages/Supplier/Supplier Profile/SupplierProfile";
 import Success from "../pages/Customer/Cart/success";
 import Cancel from "../pages/Customer/Cart/cancel";
 import CustomerRefundRequest from "../pages/Customer/Refund/Request Refund/Customer Refund Request ";
@@ -82,39 +70,6 @@ export function PageRouting() {
                 <Route path="/SalesApprovedRefundsTable" element={<SalesApprovedRefundsTable/>}/>
                 <Route path="/SalesRefundDenialForm" element={<SalesRefundDenialForm/>}/>
                 <Route path="/SalesViewRequest" element={<SalesViewRequest/>}/>
-
-
-
-                {/*sales navbar routes*/}
-                <Route path="/pendingOrders" element={<PendingOrders/>}/>
-                <Route path="/paymentdashboard" element={<PaymentDashboard/>}/>
-                <Route path="/customerHome" element={<CustomerHome/>}/>
-                <Route path="/viewRefundRequests" element={<SalesRefundRequestsTable/>}/>
-                <Route path="/discountdashboard" element={<DiscountDashboard/>}/>
-                <Route path="/logout" element={<Login/>}/>
-
-
-                {/*Customer navbar routes*/}
-                <Route path="/customerHome" element={<CustomerHome/>}/>
-                <Route path="/products" element={<Catalog/>}/>
-                <Route path="/cart" element={<Cart/>}/>
-                <Route path="/customerProfile" element={<CustomerProfile/>}/>
-                <Route path="/refundRequests" element={<CustomerRefunds/>}/>
-                <Route path="/logout" element={<Login/>}/>
-
-
-                {/*Supplier navbar routes*/}
-                <Route path="/supplierDashboard" element={<InventoryDashboard/>}/>
-                <Route path="/supplierProfile" element={<SupplierProfile/>}/>
-                <Route path="/logout" element={<Login/>}/>
-
-                {/*Inventory navbar routes*/}
-                <Route path="/viewInventory" element={<ViewInventory/>}/>
-                <Route path="/viewSupplier" element={<ViewSupplier/>}/>
-                <Route path="/purchasedOrder" element={<PurchaseOrderDashboard/>}/>
-                <Route path="/InventoryRefundRequestsTable" element={<InventoryRefundRequestsTable/>}/>
-                <Route path="/paymentsDashboard" element={<PaymentDashboard/>}/>
-                <Route path="/logout" element={<Login/>}/>
 
             </Routes>
         </div>
