ed2a3d9be4462a1e4ca52fddd016269a13eaabf6
Kulathunga D.M.D.T. <kulathungadmdt.21@uom.lk>
2024-05-03 04:01:35 +0530

    signup form is validated

--- src/pages/login & SignUp/SignUp/signup.js
+++ src/pages/login & SignUp/SignUp/signup.js
@@ -1,19 +1,80 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import "./signup.css";
 import TextField from "@mui/material/TextField";
 import {Link} from "react-router-dom";
 import CustomizedButton from "../../../components/Button/button";
 import PasswordField from "../../../components/Form Inputs/passwordField";
 
+ 
 const SignUp = () => {
 
-    const [showPassword, setShowPassword] = React.useState(false);
+const [formValues,setFormValues] = useState({});
+const [formErrors, setFormErrors] = useState({});
+//const [isSubmit, setIsSubmit] = useState(false);
 
-    const handleClickShowPassword = () => setShowPassword((show) => !show);
+const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormValues({ ...formValues, [name]: value });
+};
 
-    const handleMouseDownPassword = (event) => {
-        event.preventDefault();
-    };
+
+const handleSubmit = (e) => {
+    e.preventDefault();
+    setFormErrors(validate(formValues));
+   // setIsSubmit(true);
+};
+
+/*useEffect(() => {
+    console.log(formErrors);
+   if(Object.keys(formErrors).length === 0 && isSubmit) {
+    console.log(formValues);
+   }
+},[formErrors, formValues, isSubmit]);    */
+
+const validate = (values) =>{
+  const errors = {};
+  const email_pattern=/^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  const password_pattern= /^(?=.\d)(?=.[a-z])(?=.*[A-Z])[a-zA-z0-9]{8,}$/;
+  const contactNo_pattern =  /^\d{10}$/;
+
+  if (!values.username){
+     errors.username="Username is required!";
+  }
+
+  if (!values.email){
+    errors.email="Email is required!";
+ } else if(!email_pattern.test(values.email)) {
+    errors.email="This is not valid email format!";
+ }
+
+ if (!values.contactNo){
+    errors.contactNo="Contact No is required!";
+ } else if(!contactNo_pattern.test(values.contactNo)) {
+    errors.contactNo="This is not valid email format!";
+ } else if(isNaN(values.contactNo)) {
+    errors.contactNo="This is not a valid contact number!";
+ }
+
+
+ if (!values.Password){
+    errors.Password="Password is required!";
+ } else if(!password_pattern.test(values.password.length)) {
+    errors.password="Password is not!";
+ } else if(values.password.length < 6) {
+    errors.Password="Password must be more than 6 characters";
+ } else if(values.password.length > 8) {
+    errors.Password="Password cannot exceed more than 8 characters";
+ }
+
+
+ if (!values.confirmPassword){
+    errors.confirmPassword="Confirm Password is required!";
+ } else if (values.confirmPassword !== values.password) {
+    errors.confirmPassword = "Passwords do not match!";
+}
+  return errors;
+};
+
 
 
     return (
@@ -27,7 +88,14 @@ const SignUp = () => {
             <div className='S-RightContainer'>
                 <div className="SignupForm">
                     <h2 id="Signup">SignUp</h2>
-                    <form >
+
+            {/* {Object.keys(formErrors).length === 0 && isSubmit ? (
+            <div className="ui message success">Signed in Successfully!</div>
+            ): (
+                <pre>{JSON.stringify(formValues,undefined,2)}</pre>  
+            )} */}
+                    
+                    <form onSubmit={handleSubmit}>
                         <div className="SignupInnerContainer">
                             <div className="row">
                                 <label style={{paddingRight: "60px"}}> Username: </label>
@@ -37,9 +105,11 @@ const SignUp = () => {
                                     id="outlined-required"
                                     label="Username"
                                     required
+                                    value={formValues.username}
+                                    onChange={handleChange}
                                 />
                             </div>
-
+                            <p>{ formErrors.username}</p>
 
 
                             <div className="row">
@@ -51,9 +121,11 @@ const SignUp = () => {
                                     label="Email"
                                     type="email"
                                     required
+                                    value={formValues.email}
+                                    onChange={handleChange}
                                 />
                             </div>
-
+                            <p>{ formErrors.email}</p>
 
                             <div className="row">
                                 <label style={{paddingRight: "50px"}}> Contact No: </label>
@@ -64,19 +136,25 @@ const SignUp = () => {
                                     label="Contact No"
                                     type="number"
                                     required
+                                    value={formValues.contactNo}
+                                    onChange={handleChange}
                                 />
                             </div>
+                            <p>{ formErrors.contactNo}</p>
 
                             <div className="row">
                                 <label>Password: </label>
                                 <PasswordField
                                     placeholder="Password"
                                     style={{width:'15.25em', marginLeft: '1em'}}
-                                    showPassword={showPassword}
+                                   /* showPassword={showPassword}
                                     handleClickShowPassword={handleClickShowPassword}
-                                    handleMouseDownPassword={handleMouseDownPassword}
+                                    handleMouseDownPassword={handleMouseDownPassword} */
+                                    value={formValues.password}
+                                    onChange={handleChange}
                                 />
                             </div>
+                            <p>{ formErrors.password}</p>
 
 
                             <div className="row">
@@ -84,11 +162,14 @@ const SignUp = () => {
                                 <PasswordField
                                     placeholder="Password"
                                     style={{width:'15.25em', marginLeft: '1em'}}
-                                    showPassword={showPassword}
+                                   /* showPassword={showPassword}
                                     handleClickShowPassword={handleClickShowPassword}
-                                    handleMouseDownPassword={handleMouseDownPassword}
+                                    handleMouseDownPassword={handleMouseDownPassword} */
+                                    value={formValues.confirmPassword}
+                                    onChange={handleChange}
                                 />
                             </div>
+                            <p>{ formErrors.confirmPassword}</p>
 
 
                             <div className="btn-row">
