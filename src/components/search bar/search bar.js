df2dfec93a4cf1b09a6bc35decf8dcac882b9543
dinukamalitha <dmcoder01@gmail.com>
2024-05-02 22:22:18 +0530

    searchbar modified

--- src/components/search bar/search bar.js
+++ src/components/search bar/search bar.js
@@ -1,10 +1,19 @@
 import TextField from "@mui/material/TextField";
 import Box from "@mui/material/Box";
-import React, {useState} from "react";
+import React, { useState } from "react";
+import { GoSearch } from "react-icons/go";
 
-function SearchBar({ label = "Search Here", width = '17.5em', height = '1.95em', hoverCursor = 'pointer', defaultCursor = 'default' }){
+function SearchBar({
+                       label = "Search Here",
+                       width = '17.5em',
+                       height = '1.95em',
+                       hoverCursor = 'pointer',
+                       defaultCursor = 'default',
+                       onKeyPress
+                   }) {
 
     const [isHovered, setIsHovered] = useState(false);
+    const [query, setQuery] = useState('');
 
     const handleMouseEnter = () => {
         setIsHovered(true);
@@ -14,37 +23,64 @@ function SearchBar({ label = "Search Here", width = '17.5em', height = '1.95em',
         setIsHovered(false);
     };
 
-    return(
+    const handleChange = (event) => {
+        setQuery(event.target.value);
+    };
+
+    const handleKeyPress = (e) => {
+        if (e.key === 'Enter') {
+            e.preventDefault();
+            onKeyPress(query);
+        }
+    };
+
+    return (
         <Box
             component="form"
             sx={{
                 '& > :not(style)': {
                     m: 2,
                     width: '17.5em',
-                    "& .MuiInputBase-root":{
+                    "& .MuiInputBase-root": {
                         height: '1.95em',
                         borderRadius: '1.5em',
-                        backgroundColor: 'white'
+                        backgroundColor: 'white',
+                        position: 'relative',
                     },
                     "& .MuiInputLabel-root": {
                         fontSize: '0.6em',
                         textAlign: 'center',
                     },
+                    "& .search-icon": {
+                        position: 'absolute',
+                        right: '15px',
+                        top: '50%',
+                        transform: 'translateY(-50%)',
+                        pointerEvents: 'none',
+                    },
                 },
             }}
             noValidate
             autoComplete="on"
-            style={{display: 'flex', justifyContent: 'right'}}
+            style={{ display: 'flex', justifyContent: 'right' }}
         >
             <TextField
                 id="standard-basic"
                 label={label}
                 variant="outlined"
                 size="small"
+                value={query}
+                onChange={handleChange}
+                onKeyPress={handleKeyPress}
                 onMouseEnter={handleMouseEnter}
                 onMouseLeave={handleMouseLeave}
                 style={{
                     cursor: isHovered ? hoverCursor : defaultCursor,
+                }}
+                InputProps={{
+                    endAdornment: (
+                        <GoSearch className="search-icon" />
+                    )
                 }}
             />
         </Box>
