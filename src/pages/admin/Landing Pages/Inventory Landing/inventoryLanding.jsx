a294e59a83cacb43034f4ba9f86261b67ddbd736
dinukamalitha <dmcoder01@gmail.com>
2024-05-17 12:31:19 +0530

    admin dashboard created

--- src/pages/admin/Landing Pages/Inventory Landing/inventoryLanding.jsx
+++ src/pages/admin/Landing Pages/Inventory Landing/inventoryLanding.jsx
@@ -1,48 +1,72 @@
-import React from "react";
-import "./inventoryLanding.css"
-import InventoryLandingCard from "../../../../components/Cards/LandingPageCard";
-import InventoryNavbar from "../../../../layout/navbar/Inventory navbar/Inventory navbar";
-import Footer from "../../../../layout/footer/footer";
+//import React from "react";
+// import "./inventoryLanding.css"
+// import InventoryLandingCard from "../../../../components/Cards/LandingPageCard";
+// import InventoryNavbar from "../../../../layout/navbar/Inventory navbar/Inventory navbar";
+// import Footer from "../../../../layout/footer/footer";
+//
+// function InventoryLanding(){
+//
+//     //There should be a useEffect hook for display the inventory values in the component
+//
+//     return(
+//         <>
+//             <InventoryNavbar/>
+//
+//             <div className="inventoryLandingOuter">
+//                 <div className="inventoryLandingInner">
+//                     <div className="cardRow">
+//                         <div className="landingCard">
+//                             <InventoryLandingCard
+//                                 topic="All Item Categories" inventoryValues="25" textColor="#3F72AF">
+//                             </InventoryLandingCard>
+//                         </div>
+//                         <div className="landingCard">
+//                             <InventoryLandingCard
+//                                 topic="All Items" inventoryValues="125" textColor="#E74646">
+//                             </InventoryLandingCard>
+//                         </div>
+//                     </div>
+//                     <div className="cardRow">
+//                         <div className="landingCard secondCardRow">
+//                             <InventoryLandingCard
+//                                 topic="Low Stock Item" inventoryValues="2" textColor="#3F72AF">
+//                             </InventoryLandingCard>
+//                         </div>
+//                         <div className="landingCard secondCardRow">
+//                             <InventoryLandingCard
+//                                 topic="Overall Stock Status" inventoryValues="Healthy" textColor="#1FAB89">
+//                             </InventoryLandingCard>
+//                         </div>
+//                     </div>
+//                 </div>
+//             </div>
+//
+//             <Footer/>
+//         </>
+//     )
+// }
+//
+// export default InventoryLanding;
 
-function InventoryLanding(){
+import { useState } from 'react'
+import './inventoryLanding.css'
+import Header from '../../../../layout/adminPanel/header'
+import Sidebar from '../../../../layout/adminPanel/sidebar'
+import Home from '../../../../layout/adminPanel/home'
 
-    //There should be a useEffect hook for display the inventory values in the component
+function InventoryLanding() {
+    const [openSidebarToggle, setOpenSidebarToggle] = useState(false)
 
-    return(
-        <>
-            <InventoryNavbar/>
+    const OpenSidebar = () => {
+        setOpenSidebarToggle(!openSidebarToggle)
+    }
 
-            <div className="inventoryLandingOuter">
-                <div className="inventoryLandingInner">
-                    <div className="cardRow">
-                        <div className="landingCard">
-                            <InventoryLandingCard
-                                topic="All Item Categories" inventoryValues="25" textColor="#3F72AF">
-                            </InventoryLandingCard>
-                        </div>
-                        <div className="landingCard">
-                            <InventoryLandingCard
-                                topic="All Items" inventoryValues="125" textColor="#E74646">
-                            </InventoryLandingCard>
-                        </div>
-                    </div>
-                    <div className="cardRow">
-                        <div className="landingCard secondCardRow">
-                            <InventoryLandingCard
-                                topic="Low Stock Item" inventoryValues="2" textColor="#3F72AF">
-                            </InventoryLandingCard>
-                        </div>
-                        <div className="landingCard secondCardRow">
-                            <InventoryLandingCard
-                                topic="Overall Stock Status" inventoryValues="Healthy" textColor="#1FAB89">
-                            </InventoryLandingCard>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <Footer/>
-        </>
+    return (
+        <div className='grid-container'>
+            <Header OpenSidebar={OpenSidebar}/>
+            <Sidebar openSidebarToggle={openSidebarToggle} OpenSidebar={OpenSidebar}/>
+            <Home />
+        </div>
     )
 }
 
