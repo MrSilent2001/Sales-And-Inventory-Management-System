e6bd2b18f08d1f57598c5bd84f39faf8d87a6562
jinu <hasarijithmi@gmail.com>
2024-06-08 01:23:06 +0530

    edited modal

--- src/components/Modal/modal.jsx
+++ src/components/Modal/modal.jsx
@@ -1,16 +1,36 @@
+// import { Box } from '@mui/material';
+// const CenteredModal = ({ children, style}) => {
+//     return (
+//         <Box
+//             display="flex"
+//             justifyContent="center"
+//             alignItems="center"
+//             height="100vh"
+//             style={style}
+//         >
+//                 {children}
+//         </Box>
+//     );
+// };
+//
+// export default CenteredModal;
+
+import React, { forwardRef } from 'react';
 import { Box } from '@mui/material';
-const CenteredModal = ({ children, style}) => {
+
+const CenteredModal = forwardRef(({ children, style }, ref) => {
     return (
         <Box
+            ref={ref}
             display="flex"
             justifyContent="center"
             alignItems="center"
             height="100vh"
             style={style}
         >
-                {children}
+            {children}
         </Box>
     );
-};
+});
 
 export default CenteredModal;
