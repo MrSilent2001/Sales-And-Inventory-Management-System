09d122612f96f1ce7b9e47c74a92783d86caef72
Thikshana Ekanayake <110461079+Thikshana-Ekanayake@users.noreply.github.com>
2024-07-01 20:30:56 +0530

    fix the issue in side bar in order management

--- src/layout/sidebar/salesOrderSidebar.jsx
+++ src/layout/sidebar/salesOrderSidebar.jsx
@@ -1,26 +1,35 @@
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import CustomizedButton from "../../components/Button/button";
-import React, {useState} from "react";
+import React, { useState, useEffect } from "react";
 
-
-function SalesOrderSidebar(){
+function SalesOrderSidebar() {
     const [activeButton, setActiveButton] = useState(null);
+
+    useEffect(() => {
+        const storedActiveButton = localStorage.getItem("activeButton");
+        if (storedActiveButton) {
+            setActiveButton(storedActiveButton);
+        }
+    }, []);
+
     const handleButtonClick = (buttonText) => {
         setActiveButton(buttonText);
+        localStorage.setItem("activeButton", buttonText);
     };
+
     return (
         <div className="sales-order-sidebar">
             <div className="Button1">
                 <Link to="/pendingOrders">
                     <CustomizedButton
                         children="Pending Orders"
-                        defaultTextColor="#646FD4"
                         onClick={() => handleButtonClick("Button 1")}
+                        defaultTextColor={activeButton === "Button 1" ? 'white' : '#1565bf'}
                         variant="contained"
                         size="large"
                         style={{
-                            color: '#646FD4',
-                            backgroundColor: activeButton === "Button 1" ? 'lightblue' : 'white',
+                            color: activeButton === "Button 1" ? 'white' : '#1565bf',
+                            backgroundColor: activeButton === "Button 1" ? '#1565bf' : 'white',
                             width: '11.8em',
                             height: '3em',
                             fontSize: '0.95em',
@@ -34,22 +43,21 @@ function SalesOrderSidebar(){
                             textAlign: 'center',
                             padding: '2.2em 2.2em',
                             lineHeight: '1.4em',
-                        }}/>
+                        }} />
                 </Link>
-
             </div>
 
             <div className="Button1">
                 <Link to="/orderStatus">
                     <CustomizedButton
                         children="Update Order Status"
-                        defaultTextColor="#646FD4"
-                        onClick={() => handleButtonClick("Button 1")}
+                        onClick={() => handleButtonClick("Button 2")}
+                        defaultTextColor={activeButton === "Button 2" ? 'white' : '#1565bf'}
                         variant="contained"
                         size="large"
                         style={{
-                            color: '#646FD4',
-                            backgroundColor: activeButton === "Button 1" ? 'lightblue' : 'white',
+                            color: activeButton === "Button 2" ? 'white' : '#1565bf',
+                            backgroundColor: activeButton === "Button 2" ? '#1565bf' : 'white',
                             width: '11.8em',
                             height: '3em',
                             fontSize: '0.95em',
@@ -63,22 +71,21 @@ function SalesOrderSidebar(){
                             textAlign: 'center',
                             padding: '2.2em 2.2em',
                             lineHeight: '1.4em',
-                        }}/>
+                        }} />
                 </Link>
-
             </div>
 
             <div className="Button1">
                 <Link to="/orderDetails">
                     <CustomizedButton
                         children="Update Order Details"
-                        defaultTextColor="#646FD4"
-                        onClick={() => handleButtonClick("Button 1")}
+                        onClick={() => handleButtonClick("Button 3")}
+                        defaultTextColor={activeButton === "Button 3" ? 'white' : '#1565bf'}
                         variant="contained"
                         size="large"
                         style={{
-                            color: '#646FD4',
-                            backgroundColor: activeButton === "Button 1" ? 'lightblue' : 'white',
+                            color: activeButton === "Button 3" ? 'white' : '#1565bf',
+                            backgroundColor: activeButton === "Button 3" ? '#1565bf' : 'white',
                             width: '11.8em',
                             height: '3em',
                             fontSize: '0.95em',
@@ -92,22 +99,21 @@ function SalesOrderSidebar(){
                             textAlign: 'center',
                             padding: '2.2em 2.2em',
                             lineHeight: '1.4em',
-                        }}/>
+                        }} />
                 </Link>
-
             </div>
 
             <div className="Button1">
                 <Link to="/cancelOrders">
                     <CustomizedButton
                         children="Cancel Order"
-                        onClick={() => handleButtonClick("Button 1")}
-                        defaultTextColor="#646FD4"
+                        onClick={() => handleButtonClick("Button 4")}
+                        defaultTextColor={activeButton === "Button 4" ? 'white' : '#1565bf'}
                         variant="contained"
                         size="large"
                         style={{
-                            color: '#646FD4',
-                            backgroundColor: activeButton === "Button 1" ? 'lightblue' : 'white',
+                            color: activeButton === "Button 4" ? 'white' : '#1565bf',
+                            backgroundColor: activeButton === "Button 4" ? '#1565bf' : 'white',
                             width: '11.8em',
                             height: '3em',
                             fontSize: '0.95em',
@@ -121,13 +127,11 @@ function SalesOrderSidebar(){
                             textAlign: 'center',
                             padding: '2.2em 2.2em',
                             lineHeight: '1.4em',
-                        }}/>
+                        }} />
                 </Link>
-
             </div>
         </div>
     )
-
 }
 
 export default SalesOrderSidebar;
